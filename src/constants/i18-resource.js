626df3562baf9369c719a84d1497698f5e6b0ae8
Minho.Yoo <paromix@gmail.com>
2020-07-15 22:56:46 +0900

    영어 버전 사용 안함

--- src/constants/i18-resource.js
+++ src/constants/i18-resource.js
@@ -1,11 +1,13 @@
-import en from "./i18-en";
+//import en from "./i18-en";
 import es from "./i18-es";
 import ko from "./i18-ko";
 
 export default {
+  /*
   en: {
     translation: en
   },
+  */
   es: {
     translation: es
   },
