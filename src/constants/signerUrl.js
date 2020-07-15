695f757749531d232d57b76a340ea2695717b8ad
Minho.Yoo <paromix@gmail.com>
2020-07-15 14:27:36 +0900

    set default signer as uport

--- src/constants/signerUrl.js
+++ src/constants/signerUrl.js
@@ -1,10 +1,13 @@
 import SIGNER_STAGE from "./signer.stage";
 import SIGNER_PROD from "./signer.prod";
 
+//const _DEFAULT_SIGNER = "http://localhost:3001/"
+const _DEFAULT_SIGNER = SIGNER_STAGE
+
 const getSignerUrl = () => process.env.REACT_APP_TARGET_ENV === "prod"
   ? SIGNER_PROD
   : process.env.REACT_APP_TARGET_ENV === "stage"
     ? SIGNER_STAGE
-    : "http://localhost:3001/";
+    : _DEFAULT_SIGNER;
 
 export default getSignerUrl;
